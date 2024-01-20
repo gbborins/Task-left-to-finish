df327c04bf92688c435fc58f11e2f0fb99d6f500
Gbborins <gabreilbortolin2007@gmail.com>
2024-01-20 19:51:37 -0300

    update 0.3

--- list.js
+++ list.js
@@ -2,35 +2,58 @@ const task = document.querySelector('.input-text')
 const send = document.querySelector('.send')
 const pen = document.querySelector('.task-container')
 const con = document.querySelector('.title2')
+const body = document.querySelector('.html')
 var i = 0
 send.addEventListener("click", resp);
-send.addEventListener("keypress", resp)
-
+body.addEventListener("keypress", function(a){
+    if (a.key === "Enter"){
+        a.preventDefault();
+        resp()}
+    else if(a.code === "Space"){
+        a.preventDefault();
+    } else if(65 < a.keycode < 91){
+    a.preventDefault();
+    }task.value += String.fromCharCode(a.keyCode)
+})
+body.addEventListener('keydown', function(c){
+if(c.code === "Backspace"){
+task.value = task.value.slice(0, -1);
+c.preventDefault();
+}
+})
 function resp() {
-    let pa = document.createElement('h2');
-    pa.innerHTML = task.value;
-    pen.appendChild(pa);
-    let ta1 = document.createElement('button');
-    let im1 = document.createElement('img');
-    ta1.type = "button";
-    ta1.id = "input-concluid";
-    pa.appendChild(ta1);
-    im1.src = "https://i.pinimg.com/originals/47/05/a8/4705a8ab7c03092ef04ec4ec578493e1.jpg";
-    im1.className = "image"
-    ta1.appendChild(im1);
-    let ta2 = document.createElement('button')
-    let im2 = document.createElement('img')
-    ta2.type = "button";
-    ta2.id = "input-concluid";
-    pa.appendChild(ta2);
-    im2.src = "https://i.pinimg.com/originals/74/e0/8b/74e08b79ca412fb2462f7a7500cf91f8.jpg";
-    im2.className = "image";
-    ta2.appendChild(im2);
-    let ta3 = document.createElement('button')
-    let im3 = document.createElement('img')
-    ta3.type = "button";
-    ta3.id = "input-concluid";
-    pa.appendChild(ta3);
-    im3.src = "https://i.pinimg.com/736x/34/f3/b1/34f3b1a55e5e38f6c50910300fbd5639.jpg";
-    im3.className = "image";
-    ta3.appendChild(im3)}
+    let pa = {0: document.createElement('h2')};
+    pa[i] = document.createElement('h2');
+    pa[i].innerHTML = task.value;
+    pen.appendChild(pa[i]);
+    let right = {0:document.createElement('button')};
+    let im1 = {0:document.createElement('img')};
+    right[i] = document.createElement('button');
+    im1[i] = document.createElement('img')
+    right[i].type = "button";
+    right[i].id = "input-concluid";
+    pa[i].appendChild(right[i]);
+    im1[i].src = "https://i.pinimg.com/originals/47/05/a8/4705a8ab7c03092ef04ec4ec578493e1.jpg";
+    im1[i].className = "image"
+    right[i].appendChild(im1[i]);
+    let edit = {0:document.createElement('button')}
+    let im2 = {0:document.createElement('img')}
+    edit[i] = document.createElement('button');
+    im2[i] = document.createElement('img')
+    edit[i].type = "button";
+    edit[i].id = "input-concluid";
+    pa[i].appendChild(edit[i]);
+    im2[i].src = "https://i.pinimg.com/originals/74/e0/8b/74e08b79ca412fb2462f7a7500cf91f8.jpg";
+    im2[i].className = "image";
+    edit[i].appendChild(im2[i]);
+    let era = {0:document.createElement('button')}
+    let im3 = {0:document.createElement('img')}
+    era[i] = document.createElement('button')
+    im3[i] = document.createElement('img')
+    era[i].type = "button";
+    era[i].id = "input-concluid";
+    pa[i].appendChild(era[i]);
+    im3[i].src = "https://i.pinimg.com/736x/34/f3/b1/34f3b1a55e5e38f6c50910300fbd5639.jpg";
+    im3[i].className = "image";
+    era[i].appendChild(im3[i])
+    i++}
